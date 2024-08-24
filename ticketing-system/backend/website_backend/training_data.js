6ccc03666df8e8e0280135d44eee1192918b9c62
Satwik Kar <satwik.k.2000@gmail.com>
2024-08-24 13:41:41 +0530

    refinements

--- ticketing-system/backend/website_backend/training_data.js
+++ ticketing-system/backend/website_backend/training_data.js
@@ -1,10 +1,13 @@
+const {MUSEUM_TICKET_BOOK_QUERY, GENERAL_INQUIRY, GREETINGS} = require("../../src/components/ChatBot/query_constants");
 const trainingdata = [
-    {text: "I want to book a museum ticket", intent: "Museum Ticket Booking"},
-    {text: "Can you help me with a ticket?", intent: "Museum Ticket Booking"},
-    {text: "Hello", intent: "Greetings"},
-    {text: "Hi there!", intent: "Greetings"},
-    {text: "Give me the number of museums i can book ticket for?", intent: "General Inquiry"},
+    {text: "I want to book a museum ticket", intent: MUSEUM_TICKET_BOOK_QUERY},
+    {text: "Can you help me with a ticket?", intent: MUSEUM_TICKET_BOOK_QUERY},
+    {text: "Hello", intent: GREETINGS},
+    {
+        text: "Hi there!", intent: {text: "Hello", intent: GREETINGS},
+    },
+    {text: "Give me the number of museums i can book ticket for?", intent: GENERAL_INQUIRY},
 
-    {text: "Tell me about the museum", intent: "General Inquiry"}
+    {text: "Tell me about the museum", intent: GENERAL_INQUIRY}
 ];
 module.exports = {intentTrainingData: trainingdata}
